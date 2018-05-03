47fa802afdffae9b11e311ced1148e64d56b5ead
James Mead <james@floehopper.org>
2018-05-03 17:06:03 +0100

    Respond with JSON

--- server.rb
+++ server.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  "French President Emmanuel Macron visited Donald Trump in Washington this week. What did the US leader receive as a gift? A. An oak sapling. B. An engraved golf club. Or C. A rare book by Voltaire"
+  { questionText: 'French President Emmanuel Macron visited Donald Trump in Washington this week. What did the US leader receive as a gift? A. An oak sapling. B. An engraved golf club. Or C. A rare book by Voltaire' }.to_json
 end
